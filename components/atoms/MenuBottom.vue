dad607d2b1fb58853ddeee95c218843e4c6cc146
tim <superibm@mail.ru>
2022-12-28 22:24:53 +0300

    Верстаю главную страницу.

---
+++ components/atoms/MenuBottom.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="menu-bottom">
+    <div
+      v-for="(item, key) in items"
+      :key="key"
+      class="menu-bottom__item"
+    >
+      <nuxt-link
+        :to="item.link"
+        class="menu-bottom__link"
+      >
+        {{ item.label }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+
+import IMenu from '@/interfaces/menu';
+
+const props = defineProps({
+  items: {
+    type: Array as PropType<Array<IMenu>>,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/components/atoms/menuBottom.scss";
+</style>
--- components/organism/Footer.vue
+++ components/organism/Footer.vue
@@ -8,12 +8,34 @@
           </div>
           <div class="footer__info">
             <p>Сравниваем онлайн-курсы по digital и IT.</p>
-            <p>Мы — каталог-отзовик курсов. Выбирайте курсы по отзывам, цене, продолжительности и другим критериям!</p>
+            <p>
+              Мы — каталог-отзовик курсов. Выбирайте курсы по отзывам,
+              цене, продолжительности и другим критериям!
+            </p>
             <p>2023 Courcesalndia ©</p>
           </div>
         </div>
         <div class="footer__side footer__side--right">
-
+          <div class="footer__menus">
+            <div class="footer__menu">
+              <div class="footer__section">
+                Школы
+              </div>
+              <MenuBottom :items="menuSchools" />
+            </div>
+            <div class="footer__menu">
+              <div class="footer__section">
+                Направления
+              </div>
+              <MenuBottom :items="menuDirections" />
+            </div>
+            <div class="footer__menu">
+              <div class="footer__section">
+                Информация
+              </div>
+              <MenuBottom :items="menuInfo" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -21,7 +43,74 @@
 </template>
 
 <script lang="ts" setup>
+import {
+  ref,
+} from 'vue';
+
 import Logo from '@/components/atoms/Logo.vue';
+import MenuBottom from '@/components/atoms/MenuBottom.vue';
+import IMenu from '@/interfaces/menu';
+
+const menuSchools = ref<IMenu[]>([
+  {
+    label: 'Skillbox',
+    link: '/school/skillbox',
+  },
+  {
+    label: 'Нетология',
+    link: '/school/netology',
+  },
+  {
+    label: 'GeekBrains',
+    link: '/school/geek-brains',
+  },
+]);
+
+const menuDirections = ref<IMenu[]>([
+  {
+    label: 'Программирование',
+    link: '/directions/programmirovanie',
+  },
+  {
+    label: 'Маркетинг',
+    link: '/directions/marketing',
+  },
+  {
+    label: 'Дизайн',
+    link: '/directions/dizayn',
+  },
+  {
+    label: 'Бизнес и управление',
+    link: '/directions/biznes-i-upravlenie',
+  },
+  {
+    label: 'Аналитика',
+    link: '/directions/analitika',
+  },
+  {
+    label: 'Игры',
+    link: '/directions/igri',
+  },
+  {
+    label: 'Другие профессии',
+    link: '/directions/drugie-professii',
+  },
+]);
+
+const menuInfo = ref<IMenu[]>([
+  {
+    label: 'Контактная информация',
+    link: '/contact-info',
+  },
+  {
+    label: 'Пользовательское соглашение',
+    link: '/agreement',
+  },
+  {
+    label: 'Политика конфиденциальности',
+    link: '/privacy-policy',
+  },
+]);
 </script>
 
 <style lang="scss">
