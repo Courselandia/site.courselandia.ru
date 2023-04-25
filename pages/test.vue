a513eff42b16f834475c7f2ac57fcb9c7add32f7
Tim <superibm@mail.ru>
2023-04-25 21:44:10 +0300

    Попытка исправить поиск.

---
+++ pages/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    HERE!
+  </div>
+</template>
+
+<style lang="scss">
+.test {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: #ff0000;
+  border: 5px solid #000000;
+  z-index: 40;
+}
+</style>
