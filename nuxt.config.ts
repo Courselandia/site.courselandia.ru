d9873e5e3231c8d22820d50889b27ddba2e8d8a3
Tim <superibm@mail.ru>
2023-09-24 13:54:53 +0300

    GCR-8 Переделать сборку проекта сайта на статику.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -133,9 +133,12 @@ export default defineNuxtConfig({
     },
   },
   nitro: {
-    prerender: {
-      concurrency: 20,
-      crawlLinks: false,
+    storage: {
+      redis: {
+        driver: 'redis',
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+      },
     },
   },
 });
