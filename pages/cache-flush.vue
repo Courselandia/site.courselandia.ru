69cfed05dec974224b1d61515fdfe5a4880a0b00
Tim <superibm@mail.ru>
2024-03-28 13:22:11 +1000

    Добавил отображение ошибок в логах при удалении кеша.

--- pages/cache-flush.vue
+++ pages/cache-flush.vue
@@ -5,11 +5,20 @@
 <script lang="ts" setup>
 const config = useRuntimeConfig();
 
-await fetch(`${config.public.siteUrl}/__nuxt_multi_cache/purge/all`, {
-  method: 'POST',
-  headers: {
-    'x-nuxt-multi-cache-token': String(config.cacheToken),
-  },
-});
-await $fetch('/api/cache/flush');
+try {
+  await fetch(`${config.public.siteUrl}/__nuxt_multi_cache/purge/all`, {
+    method: 'POST',
+    headers: {
+      'x-nuxt-multi-cache-token': String(config.cacheToken),
+    },
+  });
+} catch (error: any) {
+  console.log(`Error occurred in multi cache: ${error.message}`);
+}
+
+try {
+  await $fetch('/api/cache/flush');
+} catch (error: any) {
+  console.log(`Error occurred in store cache: ${error.message}`);
+}
 </script>
