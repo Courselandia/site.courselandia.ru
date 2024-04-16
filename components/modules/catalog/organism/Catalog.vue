3e7a057d81df0cbba498e81c87168916b406f692
Tim <superibm@mail.ru>
2024-04-16 16:57:08 +1000

    Устранение ошибки с редиректом.

--- components/modules/catalog/organism/Catalog.vue
+++ components/modules/catalog/organism/Catalog.vue
@@ -106,50 +106,6 @@
         </div>
       </div>
     </div>
-
-    <LazyClientOnly>
-      <teleport to="#catalog-filters-mobile">
-        <Filters
-          v-model:selected-direction="selectedDirection"
-          v-model:selected-rating="selectedRating"
-          v-model:selected-schools="selectedSchools"
-          v-model:selected-categories="selectedCategories"
-          v-model:selected-professions="selectedProfessions"
-          v-model:selected-teachers="selectedTeachers"
-          v-model:selected-skills="selectedSkills"
-          v-model:selected-tools="selectedTools"
-          v-model:selected-format="selectedFormat"
-          v-model:selected-levels="selectedLevels"
-          v-model:selected-prices="selectedPrices"
-          v-model:selected-durations="selectedDurations"
-          v-model:selected-credit="selectedCredit"
-          v-model:selected-free="selectedFree"
-          :price-min="priceMin"
-          :price-max="priceMax"
-          :price-step="priceStep"
-          :duration-min="durationMin"
-          :duration-max="durationMax"
-          :duration-step="durationStep"
-          :directions="directions"
-          :ratings="ratings"
-          :schools="schools"
-          :categories="categories"
-          :professions="professions"
-          :teachers="teachers"
-          :skills="skills"
-          :tools="tools"
-          :formats="formats"
-          :levels="levels"
-          :available-credit="availableCredit"
-          :available-free="availableFree"
-          mobile
-          @load-items="onLoadItems"
-          @change-prices="onChangePrices"
-          @change-durations="onChangeDurations"
-          @change="onChangeFilter"
-        />
-      </teleport>
-    </LazyClientOnly>
   </div>
 </template>
 
